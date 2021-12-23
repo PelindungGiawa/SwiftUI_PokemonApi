4a1d2bcb02dc2d086acee295c69332846d8fa91d
PelindungGiawa <61076880+PelindungGiawa@users.noreply.github.com>
2021-12-24 00:29:38 +0700

    Initial Commit

---
+++ SwiftUI_PokemonApi/SwiftUI_PokemonApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PokemonApiApp.swift
+//  SwiftUI_PokemonApi
+//
+//  Created by Pelindung Giawa on 24/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PokemonApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
